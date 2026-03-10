b03c85db06332d73129ff29f0472bc3bf4b0388a
SoftCloud079 <softcloud079@gmail.com>
2026-03-10 20:37:50 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker Installed");
+// Listen for install event
+self.addEventListener("install", (event) => {
+  console.log("SoftCloud Service Worker installed");
+  self.skipWaiting(); // immediately activate the SW
+});
+
+// Activate event
+self.addEventListener("activate", (event) => {
+  console.log("SoftCloud Service Worker activated");
+});
+
+// Fetch handler (required for some browsers to trigger PWA install)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
